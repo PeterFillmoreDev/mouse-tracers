5b90d7ed8a3728c0f1578144d0f50d5d00dd0f07
PeterFillmoreDev <peterfillmoredev@gmail.com>
2021-12-04 14:34:41 -0500

    add functionality for resolution change controls

--- control.js
+++ control.js
@@ -1,5 +1,4 @@
 const container = document.querySelector("#grid");
-let gridSize = 16;
 
 function gridArray(squareNum){
   container.style.setProperty('--grid-rows', squareNum);
@@ -8,14 +7,30 @@ function gridArray(squareNum){
     let cell = document.createElement("div");
     container.appendChild(cell).className = "square";
   };
+  
+};
+
+let allSquares = document.querySelectorAll(".square");
+function sqrChange(){
+  allSquares = document.querySelectorAll(".square");
+  allSquares.forEach(square => {
+    square.addEventListener('mouseenter', (e)=>{
+      square.style.setProperty("background-color", "honeydew");
+    });
+  });
 };
 
 gridArray(32);
+sqrChange();
 
-const allSquares = document.querySelectorAll(".square");
-
-allSquares.forEach(square => {
-  square.addEventListener('mouseenter', (e)=>{
-    square.style.setProperty("background-color", "honeydew");
+const gridRes = document.querySelector("#gridRes");
+const resCountDisplay = document.querySelector("#resCountDisplay");
+gridRes.addEventListener('mouseup', (e) => {
+  resCountDisplay.textContent = `Resolution is ${gridRes.value} squares`;
+  allSquares.forEach(square => {
+    square.style.setProperty("background-color", "crimson");
+    square.remove;
   });
+  gridArray(gridRes.value);
+  sqrChange();
 });
